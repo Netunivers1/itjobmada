5e40ef87323cbee0ada8d44f6bdb0e8d932b69f1
Hanjasoa <andriamamonjylaza@gmail.com>
2018-04-19 18:25:49 +0300

    modification poster

--- web/assets/js/admin/demandeur/demandeur.js
+++ web/assets/js/admin/demandeur/demandeur.js
@@ -1,37 +1,63 @@
-var $collectionHolder;
-var $formEixst ;
-var $addTagLink = $('<a href="#" class="btn btn-primary">Ajouter Formation</a>');
-
 $(document).ready(function() {
-    $collectionHolder = $('div.form_formation');
-    $collectionHolder.append($addTagLink);
-    $collectionHolder.data('index', $collectionHolder.find(':input').length);
-    $formEixst = $collectionHolder.data('prototype');
 
-    if( $($formEixst).length <= 1){
-        addTagForm($collectionHolder);
+    /**gestion formulaire formation**/
+    var $formationExist;
+    var $form_formation = $('div.form_formation');
+    var addFormation    = addLink('Formation');
+    var removeFormation = removeForm('Formation');
+    $form_formation.append(addFormation);
+    $form_formation.data('index', $form_formation.find(':input').length);
+    $formationExist = $form_formation.data('prototype');
+    if( $($formationExist).length <= 0){
+        addTagForm($form_formation, addFormation, removeFormation);
     }
-    $addTagLink.on('click', function(e) {
+    addFormation.on('click', function(e) {
         e.preventDefault();
-        addTagForm($collectionHolder);
+        addTagForm($form_formation, addFormation, removeFormation);
     });
+    /*** fin formulaire formation**/
+
+    /**gestion formulaire experience**/
+    var $form_experienceExist;
+    var $formExperience = $('div.form_experience');
+    var addExperience    = addLink('Experience');
+    var removeExperience = removeForm('Experience');
+    $formExperience.append(addExperience);
+    $formExperience.data('index', $formExperience.find(':input').length);
+    $form_experienceExist = $formExperience.data('prototype');
+    if( $($form_experienceExist).length <= 0){
+        addTagForm($formExperience, addExperience, removeExperience);
+    }
+    addExperience.on('click', function(e) {
+        e.preventDefault();
+        addTagForm($formExperience, addExperience, removeExperience);
+    });
+    /*** fin formulaire experience**/
 });
 
-function addTagForm($collectionHolder) {
+function addLink($label){
+    var $addLinkAdd = $('<a href="#" class="btn btn-primary">Ajouter ' + $label + '</a>');
+    return $addLinkAdd ;
+}
+
+function removeForm($name) {
+    var $removeFormA = $('<a href="#" class="btn btn-danger">Supprimer ' + $name + '</a>');
+    return $removeFormA ;
+}
+
+function addTagForm($collectionHolder, $addForm, removeForm) {
     var prototype = $collectionHolder.data('prototype');
     var index = $collectionHolder.data('index');
     var newForm = prototype.replace(/__name__/g, index).replace(index+"label__", "");
-    var $newFormLi = $('<div></div>').append(newForm);
-
+    var $newFormBlock = $('<div></div>').append(newForm);
     $collectionHolder.data('index', index + 1);
-    $addTagLink.before($newFormLi);
-    addTagFormDelete($newFormLi, index);
+    $addForm.before($newFormBlock);
+    addTagFormDelete($newFormBlock, index, removeForm);
 }
 
-function addTagFormDelete($tagForm, index) {
-    var $removeFormA = $('<a href="#" class="btn btn-danger">Supprimer Formation</a>');
-    $tagForm.append($removeFormA);
-    $removeFormA.on('click', function(e) {
+function addTagFormDelete($tagForm, index, removeLink) {
+    $tagForm.append(removeLink);
+    removeLink.on('click', function(e) {
         e.preventDefault();
         $tagForm.remove();
         index = (index - 1) ;
