e203522ff5642f4a770095bd742d3c2bcc9c8c06
manou2017 <manou@shasama.com>
2018-08-13 14:00:14 +0300

    adding listing

--- src/AdminBundle/Controller/FormationsModulairesController.php
+++ src/AdminBundle/Controller/FormationsModulairesController.php
@@ -12,8 +12,20 @@ class FormationsModulairesController extends Controller
      */
     public function indexAction()
     {
+    	$lists = [];
+    	for ($i=0; $i < 1500; $i++) { 
+    		$tmp = [
+    			"Rendering" => "$i -- Trident",
+    			"Browser" => "$i -- Internet",
+    			"Platform" => "$i -- Win 95+",
+    			"Engine" => "$i -- 4",
+    			"CSS" => "$i -- X"
+    		];
+    		$lists[] = $tmp;
+    	}
+    	$lists = [];
         return $this->render('AdminBundle:FormationsModulaires:index.html.twig', array(
-            // ...
+            "lists" => $lists
         ));
     }
 
